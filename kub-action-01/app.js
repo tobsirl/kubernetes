000e565927ce4d2ba7f0f84ac5d2bcf8d3418356
Paul Tobin <tobster111@gmail.com>
2023-11-08 18:43:28 +0000

    Change to app code

--- kub-action-01/app.js
+++ kub-action-01/app.js
@@ -5,6 +5,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.send(`
     <h1>Hello from this NodeJS app!!!</h1>
+    <h2>Version 1.0.0</h2>
     <p>It's running on Kubernetes!</p>
     <p>Try sending a request to /error and see what happens</p>
   `);
